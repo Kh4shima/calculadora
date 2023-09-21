8c123feef77b1fd749f1e2c3d75612869e1277c7
Khashima <oliveira.magalhaes@gsuite.iff.edu.br>
2023-09-21 14:42:49 -0300

    Projeito inicial da calculadora

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Calculadora em Python")
+numero1 = int (input("Informe um numero inteiro"))
+numero2 = int (input("Informe outro numero inteiro"))
+operacao = input("Informe a operação (+ - * /)")
+
